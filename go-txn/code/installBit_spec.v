93563f2e8d590b3a22e7cb1a4d9e2cfe6e82fca2
Tej Chajed <tchajed@mit.edu>
2022-04-18 10:44:34 -0400

    Fix a bunch more figures
    Also restored some text on installBit in GoTxn implementation section

--- go-txn/code/installBit_spec.v
+++
@@ -1,5 +0,0 @@
-Definition install_one_bit (src dst: u8) (bit: nat) : u8 :=
-  let b := byte_to_bits src !!! bit in
-  let dst'_bits := <[bit := new_bit]> byte_to_bits dst in
-  let dst' := bits_to_byte dst'_bits in
-  dst'.
